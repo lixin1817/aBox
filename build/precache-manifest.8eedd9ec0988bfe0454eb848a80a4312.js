a0af3213bbeff0448a2966268d616657fa81c54d
lixin <1378184032@qq.com>
2019-09-19 13:41:23 +0800

    react脚手架

---
+++ build/precache-manifest.8eedd9ec0988bfe0454eb848a80a4312.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20ac11c0944f2c491c7ea066457d803",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5888335db968c0133181",
+    "url": "/static/css/main.1ceef72d.chunk.css"
+  },
+  {
+    "revision": "fe5e81cbc044cff298c6",
+    "url": "/static/js/2.84263f43.chunk.js"
+  },
+  {
+    "revision": "5888335db968c0133181",
+    "url": "/static/js/main.70edc142.chunk.js"
+  },
+  {
+    "revision": "85244f70d9f1c7996b07",
+    "url": "/static/js/runtime~main.18b8198f.js"
+  }
+]);
